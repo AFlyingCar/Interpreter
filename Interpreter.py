4fdc94ec2c4de01c495d86807622a639d91cd381
Tyler Robbins <tyler@familyrobbins.com>
2014-02-02 14:23:08 -0800

    'BOTH SAEM' command now working. Can now compare if two arguments are similar.

--- Interpreter.py
+++ Interpreter.py
@@ -6,6 +6,7 @@
 
 import platform as plat
 import sys
+import os
 
 def visible(line):
 	try:
@@ -33,6 +34,13 @@ def gimmeh(var, val):
 	except Exception as e:
 		print e
 
+#def restart():
+#	if plat.system() == 'Windows':
+#		os.system("Interpreter.py")
+#	else:
+#		os.system("python Interpreter.py")	
+#	sys.exit()
+
 def iz(arg1, arg2=None, arg3=None, condition1=None):
 
 	while True:
@@ -55,161 +63,166 @@ def iz(arg1, arg2=None, arg3=None, condition1=None):
 			if arg1 < arg2:
 				syntaxcheck(code)
 
-		elif condition1 == "BOTH SAEM":
-			if arg1 == arg2:
-				syntaxcheck(code)
+def argrun(args1, args2):
+	if args1 == args2:
+		code = lolif["YA RLY"]
+		syntaxcheck(code)
+	else:
+		code = lolif["NO WAI"]
+		syntaxcheck(code)
 
 def syntaxcheck(code):
-	if " ".join(code.split(" ", 3)[:3]) == "I HAS A":
-		try:
-			if "ITZ" in code:
-				gimmeh(code.split(" ", 3)[3].split(" ")[0], code.split(" ", 5)[5])
+	try:
+		if " ".join(code.split(" ", 3)[:3]) == "I HAS A":
+			try:
+				if "ITZ" in code:
+					gimmeh(code.split(" ", 3)[3].split(" ")[0], code.split(" ", 5)[5])
 
-			else:
-				gimmeh(code.split(" ", 3)[3], None)
+				else:
+					gimmeh(code.split(" ", 3)[3], None)
 
-		except IndexError:
-			gimmeh(code.split(" ", 1)[1], None)
+			except IndexError:
+				gimmeh(code.split(" ", 1)[1], None)
 
-	elif code.split(" ", 1)[0] == "VISIBLE":
-		vis = visible(code.split(" ", 1)[1])
-		if vis == None:
-			vis = ''
+		elif code.split(" ", 1)[0] == "VISIBLE":
+			vis = visible(code.split(" ", 1)[1])
+			if vis == None:
+				vis = ''
 
-		print vis
+			print vis
 
-	elif code.split(" ", 1)[0] == "KTHXBYE":
-		choice = raw_input("The program is still running! Do you want to kill it?(y/n) ")
-		if choice == "y":
+		elif code.split(" ", 1)[0] == "KTHXBYE":
+			choice = raw_input("The program is still running! Do you want to kill it?(y/n) ")
+			if choice == "y":
+				sys.exit()
+
+		elif code.split(" ", 1)[0] == "KTHXSYS":
 			sys.exit()
 
-	elif code.split(" ", 1)[0] == "KTHXSYS":
-		sys.exit()
+		elif code.split(" ", 1)[0] == "GIMMEH":
+			lolinput = raw_input("")
+			gimmeh(code.split(" ", 1)[1], lolinput)
 
-	elif code.split(" ", 1)[0] == "GIMMEH":
-		lolinput = raw_input("")
-		gimmeh(code.split(" ", 1)[1], lolinput)
+		elif code.split(" ")[1] == 'IZ':
+			if code.split(" ", 1)[0] in lolvars:
+				lolvars[code.split(" ")[0]] = code.split(" ")[2]
 
-	elif code.split(" ")[1] == 'IZ':
-		if code.split(" ", 1)[0] in lolvars:
-			lolvars[code.split(" ")[0]] = code.split(" ")[2]
+			else:
+				print "NameError: name '" + line + "' is not defined." #raise NameError if variable is not defined
+
+		elif code.split(" ", 1)[0] == 'IZ':
+			i =0
+
+			for item in code.split(" ")[1:]:
+				i+=1
+				
+			iz(code.split(" ")[1])
+
+		elif " ".join(code.split(" ", 2)[:2]) == 'BOTH SAEM':
+			if code == "BOTH SAEM\n":
+				print "SyntaxError: Unexpected line break"
+
+			else:
+				arg1 = code.split(" ")[2]
+				arg2 = code.split(" ")[4]
+				bothsaem(arg1, arg2)
+	
+		elif code.split(" ", 1)[0] == 'BTW':
+			pass
+
+		elif code.split(" ", 1)[0] == 'OBTW':
+			while code != 'KTHX':
+				code = raw_input("")
+
+#		elif code == 'reboot':
+#			restart()
 
 		else:
-			print "NameError: name '" + line + "' is not defined." #raise NameError if variable is not defined
+			print "NameError: name '" + code + "' is not defined"
 
-	elif code.split(" ", 1)[0] == 'IZ':
-		i =0
-
-		for item in code.split(" ")[1:]:
-			i+=1
-		
-		
-		iz(code.split(" ")[1])
-
-	elif " ".join(code.split(" ", 2))[0] == 'BOTH SAEM':
-		if code.split(" ", len(code)-2)[len(code)-1] != "O RLY?":
-			print lolerrors["SyntaxError"]
-			condition == "OIC"
-
-		arg1 = code.split(" ")[2]
-		arg2 = code.split(" ")[4]
-		lolif = {}
-		x = 0
-
-		while condition != "OIC":
-			condition = raw_input("")
-
-			if condition == "YA RLY":
-				code = raw_input("")
-
-				if "\t\t" in code:
-					lolif.update({"YA RLY":code})
-
-				else:
-					print lolerrors["IndentationError"]
-
-			elif condition.split(" ")[0] == "MEBBE":
-				code = raw_input("")
-				
-				if "\t\t" in code:
-					condition = "MEBBE" + str(x)
-					lolif.update({condition:code})
-				else:
-					print lolerrors["IndentationError"]
-
-				x = x + 1
-
-			elif condition == "NO WAI" and "NO WAI" not in lolif:
-				code = raw_input("")
-				
-				if "\t\t" in code:
-					lolif.update({"NO WAI":code})
-				else:
-					print lolerrors["IndentationError"]
-		
-		def argrun(args1, args2):
-			if arg1 == arg2:
-				code = lolif["YA RLY"]
-				syntaxcheck(code)
-
-			else:
-				code = lolif["NO WAI"]
-				syntaxcheck(code)
-
-		for item in lolif:
-			if arg1 in lolvars:
-				if arg2 in lolvars:
-					argrun(arg1, arg2)
-#					if arg1 == arg2:
-#						code = lolif["YA RLY"]
-#						syntaxcheck(code)
-
-#					else:
-#						code = lolif["NO WAI"]
-#						syntaxcheck(code)
-
-				else:
-					argrun(arg1, arg2)
-			else:
-				argrun(arg1, arg2)
-
-#					if arg1 == arg2:
-#						code = lolif["YA RLY"]
-#			syntaxcheck(code)
-#		for item in lolif:
-#		if arg1 in lolvars:
-#			if arg2 in lolvars:
-#				if lolvars[arg1] == lolvars[arg2]:
-#					syntaxcheck(code)
-#			else:
-#				if lolvars[arg1] == arg2:
-#					syntaxcheck(code)
-#		else:
-#			if arg1 == arg2:
-#				syntaxcheck(code)
-	
-	elif code.split(" ", 1)[0] == 'BTW':
+	except Exception as e:
+#		print e
 		pass
-	
-	elif code.split(" ", 1)[0] == 'OBTW':
-		while True:
-			code = raw_input("")
-			break
 
+def biggerthan(arg1, arg2):
+	if arg1 > arg2:
+		return True
 	else:
-		print "NameError: name '" + code.split(" ", 1)[0] + "' is not defined"
+		return False
+
+def lesserthan(arg1, arg2):
+	if arg1 < arg2:
+		return True
+	else:
+		return False
+
+def bothsaem(args1, args2):
+	linecode = ''	
+	code = ''
+	line = 0
+
+	while code != 'KTHX':
+		code = raw_input("")
+		linecode += code + "\n"
+	
+	linecode = linecode.split("\n")
+	linecode.remove('')
+	print linecode
+	
+	if 'YA RLY' in linecode:
+		if args1 in lolvars:
+			args1 = lolvars[args1]
+
+		if args2 in lolvars:
+			args2 = lolvars[args2]
+
+		if args1 == args2:
+			print "equal"
+			line = linecode.index('YA RLY')
+
+			while linecode[line] != 'KTHX':
+				line += 1
+				if linecode[line] == 'NO WAI':
+					line += 2
+					print 'wrong'
+				
+				print line
+				print linecode[line]
+				syntaxcheck(linecode[line])
+
+		elif args1 != args2:
+			print "not equal"
+			line = linecode.index('NO WAI')
+
+			while linecode[line] != 'KTHX':
+				line += 1
+				if linecode[line] == 'NO WAI':
+					line += 1
+					print 'correct'
+
+				syntaxcheck(linecode[line])
+	else:
+		if "YA RLY" not in linecode:
+			print "SyntaxError: Expected 'YA RLY' in <module> at line[1]"
+#		else:
+#			print "SyntaxError: Expected 'NO WAI' in <module> at line[1]"
 
 lolkeyword = {'YEP':True, 'NOPE':False}
 lolsyntax = ["VISIBLE", "KTHXBYE", "GIMMEH", "BTW", "IZ", "I HAS A"]
-lolerrors = {"IndentationError":"Indentation Error: Expected an indented block.", "SyntaxError": "Syntax Error: Invalid syntax."}
+#lolerrors = {"IndentationError":"Indentation Error: Expected an indented block.", "SyntaxError": "Syntax Error: Invalid syntax."}
 lolvars = {}
 
 print "LOLCODE IDLE 1.0 on " + str(plat.system())
 print 'Type "copyright", "credits", or "license()" for more information'
 
 while True:
-	code = raw_input(">>> ")
+	while True:
+		code = raw_input(">>> ")
 
+		if code != "\n":
+			break
+
+	print " ".join(code.split(" ", 2)[:2])
 	syntaxcheck(code)
 
 	code = ''
